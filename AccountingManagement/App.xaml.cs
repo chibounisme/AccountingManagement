d148c91337760122e6f9db2982613802da5b7766
Mohamed Chiboub <mohamed.chiboub@medius.com>
2025-05-24 10:23:03 +0100

    Add project files.

---
+++ AccountingManagement/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AccountingManagement
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ AccountingManagement/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AccountingManagement
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
